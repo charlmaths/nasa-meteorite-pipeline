2c616ae689f8b13eca9f15c42730e75ef19d227b
charlmaths <charlzen.martinez@gmail.com>
2026-04-29 19:03:19 +0100

    feat: add a function to parse url and api call

--- pipeline/ingest.py
+++ pipeline/ingest.py
@@ -1,7 +1,19 @@
 import requests
 
-r = requests.get("https://data.nasa.gov/docs/legacy/meteorite_landings/gh4g-9sfh.json", timeout=15)
+# request variable using .get() method
+var_url = "https://data.nasa.gov/docs/legacy/meteorite_landings/gh4g-9sfh.json$limit=1000"
 
-print(r.status_code)
+
+def api_ingestor(url):
+    r = requests.get(url,timeout=5)
+    stat_code = r.status_code
+    if stat_code != 200:
+        print(f"status code error: {stat_code}")
+    else:
+        print(f"status code: {stat_code}")
+
+
+api_ingestor(var_url)
+# print(r.status_code)
 # print(r.text)
-print(r.json()[0])
+# print(r.json()[0])
