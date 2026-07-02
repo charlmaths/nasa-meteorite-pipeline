f472c52e253cd4f538f1473d8197979a5a0ae8ad
charlmaths <charlzen.martinez@gmail.com>
2026-07-02 13:35:12 +0100

    fix: update null check method, implemented for loop approach

--- pipeline/data_quality.py
+++ pipeline/data_quality.py
@@ -9,47 +9,89 @@ import json
 # absolute_path:
 file_path = r"/Users/gat-x131/Documents/GitHub/DE_Project/nasa-meteorite-pipeline/data/nasa-json-api/meteor_data.json"
 
-# Parser:
-def json_parser(payload):
-    '''Parser function:
-    The idea is to parse the json file first'''
-    pl = payload
-    pl_list = []
-    pl_dict = {}
-    for item in pl:
-        pl_list.append(item)
-        # pl_dict[item]
-
-    # print(pl_list)
-    print(pl_list[0])
-    # print(pl_dict.items())
-
-
-# Using with open to use all the methods above, this will go into a main function later
-with open(file_path, "r") as f:
-    data = json.load(f)
-    # print(type(data))
-    json_parser(data)
-
-
-
-with open(file_path) as file:
-    json_parser(json.load(file))
-
-
-# json_parser(file_path)
 
 # Null check:
+def null_check(payload):
+    ''' Simple null check -
+    using for loop approach, it is much cleaner and we don't have to worry about iterating [i]
+
+    using a while loop, this method goes through each element and the item inside that element.
+    if conditions are met, the item is the result is appended into a list. Null checks each field.
+
+    '''
+
+    id_null_list = []
+
+    ###### for loop approach: #####
+    for record in payload:
+        if record["id"] is None or record["id"].strip() == "":
+            id_null_list.append(record["name"])
 
 
-# Year check:
+    ###### while loop approach: #####
+
+    # i = 0
+    # id_null_list = []
+
+    # while i < len(payload):
+
+        # test_list.append(payload[i]["name"]) # Brings i element, and the value for "name" key
+        # test_list.append(payload[i]["recclass"]) # Brings i element, and the value for "reclass" key
+        # test_list.append(payload[i]["id"]) # Brings i element, and the value for "id" key
+        # if payload[i]["nametype"] is None or payload[i]["nametype"] == "":
+        #     test_list.append(payload[i])
 
 
-# Mass check:
+        # if payload[i]["id"] is None or payload[i]["id"].strip() == "":
+        #     id_null_list.append(payload[i]["name"])
+            # return f"No id found for meteor: {payload[i]["name"]}"
+            # return f"No id found for meteor: {id_null_list}"
+
+            # test_list.append(payload[i])
+
+        # if payload[i]["recclass"].strip() is None or payload[i]["recclass"].strip() == "":
+            # recclass_null_list.append(payload[i]["name"])
+            # return f"No recclass founds for meteor: {payload[i]["name"]}"
+
+        # i += 1
+    # print(test_list)
+
+    return f"No id found on meteors: {id_null_list}"
 
 
-# Coordinate check:
 
 
-# Failed files to qurantine:
+# # Year check:
+# def year_check(payload):
+#     pass
+
+# # Mass check:
+# def mass_check(payload):
+#     pass
+
+# # Coordinate check:
+# def coord_check(payload):
+#     pass
+
+# # Failed files to qurantine:
+# def quarantine_check(payload):
+
+# Using 'with open' to use all the methods above, this will go into a main function later
+with open(file_path, "r") as f:
+    data = json.load(f)
+    # print(data[0]["id"]) # Accessing the list first [] then dictionary keys ["keys"]
+    # print(type(data))
+
+    # method calls
+    # json_parser(data)
+    print(null_check(data))
+
+
+
+
+
+
+
+
+
 
